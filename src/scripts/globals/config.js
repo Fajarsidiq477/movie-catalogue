00ebfbb193e18f2616b732e8b8c9c2082fef740b
Fajar <fajars04@outlook.com>
2023-04-28 10:33:19 +0700

    change api key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '5cc6a8ed38f59f7fca4eac2fba3ef244',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
